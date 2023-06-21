861b9e7512c4fc963a14ea213028108c2c5adaba
shriyeole <shrijeetyeole2@gmail.com>
2023-06-21 11:02:53 +0530

    added

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
@@ -6,50 +7,45 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  
+  currentIndex: number = 0;
+  selectedAnswer: string | undefined;
+  score: number = 0;
+  showResult: boolean = false;
 
-  constructor() {
-    
-  }
-  alphabets: string[] = [
-    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
-    'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+  questions: any[] = [
+    {
+      question: 'What is the capital of France?',
+      options: ['Paris', 'London', 'Madrid', 'Rome'],
+      answer: 'Paris',
+    },
+    {
+      question: 'Which planet is known as the Red Planet?',
+      options: ['Mars', 'Venus', 'Mercury', 'Jupiter'],
+      answer: 'Mars',
+    },
+    // Add more questions here...
   ];
-  selectedWord: string | null = null;
 
-  getWordByAlphabet(alphabet: string): string {
-    const words: { [key: string]: string } = {
-      A: 'Apple',
-      B: 'Banana',
-      C: 'Cat',
-      D: 'Dog',
-      E: 'Elephant',
-      F: 'Fish',
-      G: 'Giraffe',
-      H: 'Horse',
-      I: 'Ice Cream',
-      J: 'Jellyfish',
-      K: 'Kangaroo',
-      L: 'Lion',
-      M: 'Monkey',
-      N: 'Nightingale',
-      O: 'Octopus',
-      P: 'Penguin',
-      Q: 'Queen',
-      R: 'Rabbit',
-      S: 'Sunflower',
-      T: 'Tiger',
-      U: 'Umbrella',
-      V: 'Violet',
-      W: 'Whale',
-      X: 'Xylophone',
-      Y: 'Yak',
-      Z: 'Zebra'
-    };
+  constructor(private alertController: AlertController) {}
 
-    return words[alphabet] || 'No word found';
+  checkAnswer() {
+    if (this.selectedAnswer === this.questions[this.currentIndex].answer) {
+      this.score++;
+    }
+
+    if (this.currentIndex === this.questions.length - 1) {
+      this.showResult = true;
+    } else {
+      this.currentIndex++;
+      this.selectedAnswer ;
+    }
   }
 
-  displayWord(alphabet: string) {
-    this.selectedWord = alphabet;
+  async restartQuiz() {
+    this.currentIndex = 0;
+    this.selectedAnswer ;
+    this.score = 0;
+    this.showResult = false;
   }
 }
